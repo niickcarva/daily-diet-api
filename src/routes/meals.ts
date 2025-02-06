84d7a5fa3c37466d26a22610adab3ed1e3b23e2e
niickcarva <nicolasc.avelaneda@gmail.com>
2025-02-06 20:01:45 -0300

    feat: implement get and delete meal endpoints

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -12,43 +12,52 @@ const createUpdateMealSchema = z.object({
 });
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.get("/", { preHandler: [checkUserIdExists] }, async (request) => {
+  app.addHook("preHandler", checkUserIdExists);
+
+  app.get("/", async (request) => {
     const { userId } = request.cookies;
     const meals = await knex("meals").select("*").where({ user_id: userId });
     return { meals };
   });
 
-  app.put(
-    "/:id",
-    { preHandler: [checkUserIdExists] },
-    async (request, reply) => {
-      const { userId } = request.cookies;
-      const mealId = (request.params as { id: string }).id;
+  app.get("/:id", async (request) => {
+    const { userId } = request.cookies;
+    const mealId = (request.params as { id: string }).id;
 
-      const existingMeal = await knex("meals")
-        .select("*")
-        .where({
-          id: mealId,
-          user_id: userId,
-        })
-        .first();
-      if (!existingMeal) {
-        return reply.status(404).send({ error: "Meal not found" });
-      }
+    const meal = await knex("meals")
+      .select("*")
+      .where({ id: mealId, user_id: userId })
+      .first();
+    return { meal };
+  });
 
-      const { name, description, is_diet } = createUpdateMealSchema.parse(
-        request.body
-      );
+  app.put("/:id", async (request, reply) => {
+    const { userId } = request.cookies;
+    const mealId = (request.params as { id: string }).id;
 
-      await knex("meals").where("id", mealId).update({
-        name,
-        description,
-        is_diet,
-      });
+    const existingMeal = await knex("meals")
+      .select("id")
+      .where({
+        id: mealId,
+        user_id: userId,
+      })
+      .first();
+    if (!existingMeal) {
+      return reply.status(404).send({ error: "Meal not found" });
     }
-  );
 
-  app.post("", { preHandler: [checkUserIdExists] }, async (request) => {
+    const { name, description, is_diet } = createUpdateMealSchema.parse(
+      request.body
+    );
+
+    await knex("meals").where("id", mealId).update({
+      name,
+      description,
+      is_diet,
+    });
+  });
+
+  app.post("", async (request) => {
     const { name, description, is_diet } = createUpdateMealSchema.parse(
       request.body
     );
@@ -60,6 +69,27 @@ export async function mealsRoutes(app: FastifyInstance) {
       name,
       description,
       is_diet,
+      user_id: userId,
+    });
+  });
+
+  app.delete("/:id", async (request, reply) => {
+    const { userId } = request.cookies;
+    const mealId = (request.params as { id: string }).id;
+
+    const existingMeal = await knex("meals")
+      .select("id")
+      .where({
+        id: mealId,
+        user_id: userId,
+      })
+      .first();
+    if (!existingMeal) {
+      return reply.status(404).send({ error: "Meal not found" });
+    }
+
+    await knex("meals").delete().where({
+      id: mealId,
       user_id: userId,
     });
   });
