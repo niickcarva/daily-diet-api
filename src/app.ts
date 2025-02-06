de409a39838a941e1af6db4b02a482d2519ca66e
niickcarva <nicolasc.avelaneda@gmail.com>
2025-02-06 18:37:25 -0300

    chore: add routes files

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
 import fastify from "fastify";
 import cookie from "@fastify/cookie";
+import { usersRoutes } from "./routes/users";
+import { mealsRoutes } from "./routes/meals";
 
 export const app = fastify();
 
 app.addHook("preHandler", async (request) => {
+  console.log(`--- ---`);
   console.log(`👀 [${request.method}] ${request.url}`);
+  console.time("RequestTime");
+});
+app.addHook("onResponse", async () => {
+  console.timeEnd("RequestTime");
 });
 
 app.register(cookie);
+
+app.register(usersRoutes, {
+  prefix: "/users",
+});
+app.register(mealsRoutes, {
+  prefix: "/meals",
+});
